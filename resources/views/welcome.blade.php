9cda0f8b56b304ad7face3b3df78aae255778ad4
it <s073009.git@gmail.com>
2019-07-21 02:59:05 +0000

    user registration

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -4,8 +4,10 @@
     <div class="center jumbotron">
         <div class="text-center">
             <h1>将棋の盤面の写真を投稿し、コメント及び評価できるサイト</h1>
-            <button class="btn btn-primary">会員登録</button>
+            {!! link_to_route('signup.get', '会員登録', [], ['class' => 'btn btn-lg btn-primary']) !!}
             <button class="btn btn-primary">ログイン</button>
+            
         </div>
     </div>
 @endsection
+
