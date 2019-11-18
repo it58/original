9208593d89872bf534bcba248c9fc1823c53e4b4
it <s073009.git@gmail.com>
2019-11-18 12:57:14 +0000

    テストユーザー機能編集

--- resources/views/top.blade.php
+++ resources/views/top.blade.php
@@ -5,18 +5,18 @@
         <div class="row">
             <div class="text-center mt-5">
                 <div class="center jumbotron ">
-                    <h1>将棋の盤面の写真を投稿し、コメント及び評価できるサイト</h1>
+                    <h2>将棋の盤面の写真を投稿し、コメント及び評価できるサイト</h2>
                 <!--Flex動作を有効にする、横並び、等間隔に並列-->
                     <div class="d-md-flex flex-row justify-content-around mt-5">
-                        {!! link_to_route('signup.get', '会員登録', [], ['class' => 'btn btn-md btn-primary m-1']) !!}
-                        {!! link_to_route('login', 'ログイン', [], ['class' => 'btn btn-md btn-primary m-1']) !!}
+                        {!! link_to_route('signup.get', '会員登録', [], ['class' => 'btn btn-sm btn-primary m-1']) !!}
+                        {!! link_to_route('login', 'ログイン', [], ['class' => 'btn btn-sm btn-primary m-1']) !!}
                         
                          @if(!Auth::check())
-                            <form action="{{ route('login.post') }}" method="POST" class="mt-3">
+                            <form action="{{ route('login.post') }}" method="POST">
                               {{ csrf_field() }}
                                 <input type="hidden" name="email" value="test@gmail.com">
                                 <input type="hidden" name="password" value="123456">
-                                <button type="submit" class="btn btn-primary">テストユーザーでログイン</button>
+                                <button type="submit" class="btn btn-sm btn-primary m-1">テストユーザーでログイン</button>
                             </form>
                         @endif
                     </div>
